88eab885322981d3868142fbf1d5d50c9c8da1a7
= <=>
2020-03-18 16:11:41 +0100

    first commit

---
+++ bot.py
@@ -0,0 +1,34 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+    
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f"Hola {member.name}, benvingut al servidor de l'Ajuntament de Sant Vicenç de Castellet."
+        f'Per que puguis entrar als xats del teu Departament, comenta a consultes en Informàtica o demana-ho als administradors'
+        f'Recorda marcar les entrades i sortides de la jornada laboral a travès de https://forms.gle/fTtS8RHNVmXwV7d18'
+        f'Que tinguis un bon dia.'
+    )
+
+client.run(TOKEN)
